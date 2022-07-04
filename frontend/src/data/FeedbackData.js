2773a5b377ad97fdf940c039c7a5215d39c0aee6
NicMilli <91165462+NicMilli@users.noreply.github.com>
2022-07-04 16:57:12 -0700

    backend, front imported, not connected

---
+++ frontend/src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'Example rating 1',
+      },
+      {
+        id: 2,
+        rating: 9,
+        text: 'Example rating 2',
+      },
+      {
+        id: 3,
+        rating: 8,
+        text: 'Example rating 3',
+      },
+]
+
+export default FeedbackData
