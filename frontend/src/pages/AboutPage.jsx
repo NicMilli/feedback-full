fa48ee154371387bc8bbcef8bd6bb082cc72465c
NicMilli <91165462+NicMilli@users.noreply.github.com>
2022-07-04 17:50:51 -0700

    redux added

---
+++ frontend/src/pages/AboutPage.jsx
@@ -0,0 +1,22 @@
+import Card from "../components/shared/Card"
+import {Link} from 'react-router-dom'
+
+function AboutPage() {
+  return (
+    <Card>
+        <div className="about">
+            <h1>About This Project</h1>
+            <p>This is a react app to leave feedback for my website
+                 and other projects. It has been adapted from
+                 'React Front to Back 2022' on Udemy.com</p>
+            <p>Version 1.0.0</p>
+            <p>
+                <Link to='/'>Back to the Feedabck app</Link>
+            </p>
+        </div>
+      
+    </Card>
+  )
+}
+
+export default AboutPage
